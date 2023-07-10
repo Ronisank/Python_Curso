8761bb74013a3409ddd498cea0ed051b69dd31db
Roni <ronisank21@hotmail.com>
2023-07-10 15:21:05 -0300

    Exercises_from_class_78_to_87

---
+++ aula81.py
@@ -0,0 +1,40 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+# lista = [4, 32, 1, 34, 5, 6, 6, 21, ]
+# lista.sort(reverse=True)
+# sorted(lista)
+lista = [
+    {"nome": "Luiz", "sobrenome": "miranda"},
+    {"nome": "Maria", "sobrenome": "Oliveira"},
+    {"nome": "Daniel", "sobrenome": "Silva"},
+    {"nome": "Eduardo", "sobrenome": "Moreira"},
+    {"nome": "Aline", "sobrenome": "Souza"},
+]
+
+
+def exibir(item):
+    for item in lista:
+        print(item)
+    print()
+
+
+# lista.sort(key=lambda item: item["nome"])
+# print(lista)
+l1 = sorted(lista, key=lambda item: item["nome"])
+l2 = sorted(lista, key=lambda item: item["sobrenome"])  # Cópia Rasa
+# sorted(lista) # sorted não altera a lista original, faz uma cópia rasa
+print("Ordenado por Nome\n")
+exibir(l1)
+print("Ordenado por Sobrenome\n")
+exibir(l2)
---
+++ aula84.py
@@ -0,0 +1,54 @@
+import pprint
+
+# Introdução à List comprehension em Python
+# List comprehension é uma forma rápida para criar listas
+# a partir de iteráveis.
+# print(list(range(10)))
+# lista = []
+# for numero in range(10):
+#     lista.append(numero)
+# # print(lista)
+
+# lista = [numero * 2 for numero in range(10)]
+# print(lista)
+
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=45, indent=4)
+
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {
+        "nome": "p1",
+        "preco": 20,
+    },
+    {
+        "nome": "p2",
+        "preco": 10,
+    },
+    {
+        "nome": "p3",
+        "preco": 30,
+    },
+]
+# novos_produtos = [
+#     {**produto, "preco": produto["preco"] * 1.05}
+#     if produto["preco"] > 20
+#     else {**produto}
+#     for produto in produtos
+# ]
+
+# print(novos_produtos)
+# print(*novos_produtos, sep="\n")
+
+# Filtragem de dados em list comprehension
+
+novos_produtos = [
+    {**produto, "preco": produto["preco"] * 1.05}
+    if produto["preco"] > 20
+    else {**produto}
+    for produto in produtos
+    if (produto["preco"] >= 20 and produto["preco"] * 1.05) > 10
+]
+p(novos_produtos)
